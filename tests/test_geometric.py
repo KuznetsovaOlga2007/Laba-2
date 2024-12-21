8b64151eca7cb505568a6b322e5e91f299cca9d5
KuznetsovaOlga2007 <topmodelolga@gmail.com>
2024-12-22 02:44:04 +0300

    Update test_geometric.py

--- tests/test_geometric.py
+++ tests/test_geometric.py
@@ -1,64 +1,78 @@
 import math
 import pytest
-import shapes.circle as circle
-import shapes.square as square
-import shapes.triangle as triangle
-from calculator import calculate as calc
+import circle
+import square
+import triangle
+from calculate import calc
 
 
-class CircleTests:
-    def test_area_of_circle(self):
-        radius = 3
+class TestCircle:
+    def test_circle_area(self):
+        radius = 2
         expected_area = math.pi * radius ** 2
         assert circle.area(radius) == expected_area
 
-    def test_perimeter_of_circle(self):
-        radius = 3
+    def test_circle_perimeter(self):
+        radius = 2
         expected_perimeter = 2 * math.pi * radius
         assert circle.perimeter(radius) == expected_perimeter
 
 
-class SquareTests:
-    def test_area_of_square(self):
-        assert square.area(4) == 16
-        assert square.area(3) == 9
+class TestSquare:
+    def test_square_area(self):
+        side1 = 5
+        side2 = 2
+        assert square.area(side1) == side1 ** 2
+        assert square.area(side2) == side2 ** 2
 
-    def test_perimeter_of_square(self):
-        assert square.perimeter(5) == 20
-        assert square.perimeter(3) == 12
-
-
-class TriangleTests:
-    def test_area_of_triangle(self):
-        assert triangle.area(5, 5, 5) == 10.825317547305486
-        assert triangle.area(10, 24, 22) == 120
-
-    def test_perimeter_of_triangle(self):
-        assert triangle.perimeter(5, 5, 5) == 15
-        assert triangle.perimeter(10, 24, 22) == 56
+    def test_square_perimeter(self):
+        side1 = 4
+        side2 = 2
+        assert square.perimeter(side1) == 4 * side1
+        assert square.perimeter(side2) == 4 * side2
 
 
-class CalculatorTests:
-    def test_negative_or_zero_size(self):
+class TestTriangle:
+    def test_triangle_area(self):
+        base1, height1 = 6, 6
+        expected_area1 = 15.588457268119896
+        assert triangle.area(base1, base1, base1) == expected_area1
+        
+        base2, side_a, side_b = 8, 17, 15
+        expected_area2 = 60
+        assert triangle.area(base2, side_a, side_b) == expected_area2
+
+    def test_triangle_perimeter(self):
+        side1 = 6
+        expected_perimeter1 = 18
+        assert triangle.perimeter(side1, side1, side1) == expected_perimeter1
+        
+        side_a, side_b, side_c = 8, 17, 15
+        expected_perimeter2 = 40
+        assert triangle.perimeter(side_a, side_b, side_c) == expected_perimeter2
+
+
+class TestCalc:
+    def test_not_positive_size(self):
         with pytest.raises(ValueError):
-            calc("square", "perimeter", [-4])
+            calc("square", "perimeter", [-5])
 
-    def test_valid_triangle_sides(self):
+    def test_correct_triangle_size(self):
         with pytest.raises(ValueError):
-            calc("triangle", "perimeter", [-3, 4, 0])
+            calc("triangle", "perimeter", [-5, 5, 0])
 
-    def test_invalid_triangle_side_length(self):
+    def test_correct_triangle_side_value(self):
         with pytest.raises(ValueError):
-            calc("triangle", "perimeter", [4, 3, 8])
+            calc("triangle", "perimeter", [3, 2, 5])
 
-    def test_invalid_shape_type(self):
+    def test_correct_fig(self):
         with pytest.raises(AssertionError):
-            calc("pentagon", "perimeter", [6, 6, 6])
+            calc("tr", "perimeter", [6, 6, 6])
 
-    def test_invalid_function_type(self):
+    def test_correct_func(self):
         with pytest.raises(AssertionError):
-            calc("triangle", "area_calc", [6, 6, 6])
+            calc("triangle", "per", [6, 6, 6])
 
-    def test_invalid_size_type(self):
+    def test_correct_size(self):
         with pytest.raises(TypeError):
-            calc("triangle", "perimeter", [1, "two", 3])
+            calc("triangle", "perimeter", "triangle")
