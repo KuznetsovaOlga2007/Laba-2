254360da17bf37a0e8b00dfabd942fa72de76a43
KuznetsovaOlga2007 <topmodelolga@gmail.com>
2024-12-22 04:19:36 +0300

    The file has been modified

--- src/triangle.py
+++ src/triangle.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import math 
-=======
 import math
->>>>>>> 80706558caf65c159c0047ee6770f21a5dec8062
 def area(a, b, c):
     s = (a + b + c) / 2
     return math.sqrt(s*(s - a) * (s - b) * (s - c))
